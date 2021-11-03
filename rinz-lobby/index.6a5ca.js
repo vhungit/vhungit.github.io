e8dfd66a908790db628f1b8134ff358bedc6e85c
vhungit <vhung.it@gmail.com>
2021-11-03 11:40:22 +0700

    - update

--- rinz-lobby/index.6a5ca.js
+++ rinz-lobby/index.6a5ca.js
@@ -1,4 +1,4 @@
-System.register(["./application.b6b08.js"], function (_export, _context) {
+System.register(["./application.88fac.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.b6b08.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationB6b08Js) {
-      createApplication = _applicationB6b08Js.createApplication;
+    setters: [function (_application88facJs) {
+      createApplication = _application88facJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
