ba8720ffe69ed7e2e53c7770348fe99bbb2cc078
vhungit <vhung.it@gmail.com>
2021-11-21 14:06:11 +0700

    - update

--- rinz-lobby/index.fcf80.js
+++ rinz-lobby/index.fcf80.js
@@ -1,4 +1,4 @@
-System.register(["./application.32439.js"], function (_export, _context) {
+System.register(["./application.4f0b7.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.32439.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application32439Js) {
-      createApplication = _application32439Js.createApplication;
+    setters: [function (_application4f0b7Js) {
+      createApplication = _application4f0b7Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
