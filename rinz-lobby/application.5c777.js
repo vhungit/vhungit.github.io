fd5ae159f1a0a1ee5a982a006517d19318f4c8d4
vhungit <vhung.it@gmail.com>
2021-11-28 11:58:23 +0700

    - update

--- rinz-lobby/application.5c777.js
+++ rinz-lobby/application.5c777.js
@@ -97,7 +97,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.03c53.json';
+      var settings = 'src/settings.98f45.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- rinz-lobby/index.8665a.js
+++ rinz-lobby/index.8665a.js
@@ -1,4 +1,4 @@
-System.register(["./application.9f272.js"], function (_export, _context) {
+System.register(["./application.5c777.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.9f272.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application9f272Js) {
-      createApplication = _application9f272Js.createApplication;
+    setters: [function (_application5c777Js) {
+      createApplication = _application5c777Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
