d0c5558090a6921ddce5a9141a43496d5c6c08ce
vhungit <vhung.it@gmail.com>
2022-10-10 10:31:25 +0700

    update

--- liam/farm/cocos-js/wait-for-ammo-instantiation.js
+++ liam/farm/cocos-js/wait-for-ammo-instantiation.js
@@ -1,1 +1,1 @@
-System.register(["./instantiated-eff15930.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hO)}],execute:function(){}}}));
+System.register(["./instantiated-0b081720.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hO)}],execute:function(){}}}));
